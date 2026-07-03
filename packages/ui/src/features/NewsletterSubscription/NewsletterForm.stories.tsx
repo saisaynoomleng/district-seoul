eaec28cf48759efbc28b7e4a66631c46647d9046
saisaynoomleng <saileng9723@gmail.com>
2026-07-03 18:29:55 -0400

    components_web_newsletter_subscription

---
+++ packages/ui/src/features/NewsletterSubscription/NewsletterForm.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { NewsletterForm } from './NewsletterForm';
+import { expect, fn } from 'storybook/test';
+import {
+  ActionResponse,
+  NewsletterFormSchemaValues,
+} from '@district-seoul/utils';
+
+const mockAction = fn(
+  async (
+    data: NewsletterFormSchemaValues,
+  ): Promise<ActionResponse<NewsletterFormSchemaValues>> => {
+    return {
+      success: true,
+      message: 'success',
+    };
+  },
+);
+
+const meta: Meta<typeof NewsletterForm> = {
+  title: 'Features/NewsletterForm',
+  component: NewsletterForm,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+    docs: {
+      description: {
+        component: 'Newsletter Subscription Form',
+      },
+    },
+  },
+  args: {
+    action: mockAction,
+  },
+  argTypes: {
+    action: {
+      control: false,
+      description: 'To render server action on Next.js',
+    },
+
+    className: {
+      control: 'text',
+      description: 'Additional TailwindCSS classes',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const FilledForm: Story = {
+  render: (args) => <NewsletterForm {...args} />,
+  play: async ({ canvas, userEvent }) => {
+    const input = canvas.getByLabelText(/email/i);
+    const btn = canvas.getByRole('button');
+
+    await expect(input).toBeInTheDocument();
+    await expect(btn).toBeInTheDocument();
+
+    await userEvent.type(input, 'johndoe@mail.com');
+    await userEvent.click(btn);
+
+    await expect(mockAction).toHaveBeenCalledWith({
+      email: 'johndoe@mail.com',
+    });
+  },
+};
---
+++ packages/ui/src/features/NewsletterSubscription/NewsletterForm.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  NewsletterFormSchema,
+  NewsletterFormSchemaValues,
+  ActionResponse,
+} from '@district-seoul/utils';
+import { Input } from '#components/ui/input';
+import {
+  FormErrorMessage,
+  FormLabel,
+  SubmitButton,
+} from '../../components/shared';
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+
+type NewsletterFormProps = {
+  className?: string;
+  action: (
+    data: NewsletterFormSchemaValues,
+  ) => Promise<ActionResponse<NewsletterFormSchemaValues>>;
+};
+
+export const NewsletterForm = ({
+  className,
+  action,
+}: NewsletterFormProps): React.JSX.Element => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<NewsletterFormSchemaValues>({
+    resolver: zodResolver(NewsletterFormSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<NewsletterFormSchemaValues> = async (data) => {
+    const result = await action(data);
+
+    if (!result.success) {
+      setError(result.field as keyof NewsletterFormSchemaValues, {
+        message: result.message,
+      });
+    }
+  };
+
+  return (
+    <form
+      className={twMerge(clsx('space-y-2', className))}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <FormLabel htmlFor="email">Email</FormLabel>
+      <div className="flex gap-x-2">
+        <div className="space-y-2 w-full">
+          <Input
+            type="email"
+            id="email"
+            autoComplete="email"
+            aria-describedby="email-help"
+            {...register('email')}
+            className={twMerge(
+              clsx(
+                errors.email &&
+                  'border-brand-error-500 focus-visible:ring-brand-error-500',
+              ),
+            )}
+          />
+          {errors.email && (
+            <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+          )}
+        </div>
+        <SubmitButton>Subscribe</SubmitButton>
+      </div>
+    </form>
+  );
+};
